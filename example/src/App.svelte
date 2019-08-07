8c1a7a4a1cb3e3b3c61f1ca044c7410b85440a40
Markus Felten <Markus.Felten@gmx.de>
2019-08-07 20:14:34 +0200

    test: switch to nightwatch

---
+++ example/src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  import About from "./About.svelte";
+  import Home from "./Home.svelte";
+  import Outlet from "../../src/components/Outlet.svelte";
+  import Link from "../../src/components/Link.svelte";
+  import { Router, route } from "../../src/router.mjs";
+
+  const router = new Router([route("/about", About), route("/", Home)]);
+</script>
+
+<div>
+  <Outlet {router}>nichts gefunden</Outlet>
+</div>
