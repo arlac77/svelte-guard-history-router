102c399fea2e45cafddc766d5a248eafe15a692f
Markus Felten <Markus.Felten@gmx.de>
2019-08-07 16:30:22 +0200

    test: start

---
+++ tests/src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Outlet from "../../src/components/Outlet.svelte";
+  import Link from "../../src/components/Link.svelte";
+  import { Router } from "../../src/router.mjs";
+
+  const router = new Router([
+  ]);
+</script>
+
+<div>
+  <Outlet {router}>nichts gefunden</Outlet>
+</div>
