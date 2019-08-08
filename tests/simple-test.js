818739164f7fc4b6b9607c9a1c7f713577e10691
Markus Felten <Markus.Felten@gmx.de>
2019-08-08 20:47:03 +0200

    test: article fixture

--- tests/simple-test.js
+++ tests/simple-test.js
@@ -2,7 +2,16 @@ const assert = require("assert");
 
 const base = "http://localhost:5000";
 
-const sg = (t,name='xxx') => `test-results/${t.test.fullTitle().replace(/\s+/,'-','g')}-${name}.png`
+const sg = (t, name = "xxx") =>
+  `test-results/${t.test.fullTitle().replace(/\s+/, "-", "g")}-${name}.png`;
+
+const articles = {
+  "01": { name: "Peanutbutter" },
+  "02": { name: "Cheesecake" },
+  "03": { name: "Pizza Four Seasons" },
+  "04": { name: "Pizza Salami" },
+  "05": { name: "Pizza Hawaii" }
+};
 
 describe("router", function() {
   this.slow(2000);
@@ -34,7 +43,7 @@ describe("router", function() {
     browser
       .url(`${base}/index.html`)
       .waitForElementVisible("h2.routetitle")
-      .saveScreenshot(sg(this,'index'))
+      .saveScreenshot(sg(this, "index"))
       .assert.containsText("h2.routetitle", "Home")
       .assert.urlEquals(`${base}/`);
 
@@ -44,7 +53,7 @@ describe("router", function() {
       .assert.containsText("h2.routetitle", "About")
       .assert.urlEquals(`${base}/about`);
 
-  /*
+    /*
     browser
       .url(`${base}/article/01`)
       .saveScreenshot(sg(this,'pre-article-01'))
@@ -60,19 +69,21 @@ describe("router", function() {
       .url(`${base}`)
       .waitForElementVisible('a[href="/about"]')
       .click('a[href="/about"]', () => {
-        browser.saveScreenshot(sg(this,'click-about'))
-        .assert.urlContains('/about');
+        browser
+          .saveScreenshot(sg(this, "click-about"))
+          .assert.urlContains("/about");
 
         browser
           .waitForElementVisible("h2.routetitle")
           .assert.containsText("h2.routetitle", "About")
           .click('a[href="/"]', () => {
-            browser.saveScreenshot(sg(this,'click-home'));
+            browser.saveScreenshot(sg(this, "click-home"));
             browser
               .waitForElementVisible("h2.routetitle")
               .assert.containsText("h2.routetitle", "Home")
               .click('a[href="/article/01"]', () => {
-                browser.saveScreenshot(sg(this,'click-article-01'))
+                browser
+                  .saveScreenshot(sg(this, "click-article-01"))
                   .waitForElementVisible("h2.routetitle")
                   .assert.containsText("h2.routetitle", "Article Peanutbutter")
                   .assert.urlEquals(`${base}/article/01`);
@@ -83,30 +94,29 @@ describe("router", function() {
       });
   });
 
-
   it("browse-through-articles", async function(browser) {
-    browser
-    .url(`${base}`)
-    .click('a[href="/article"]', async () => {
-      browser.saveScreenshot(sg(this,'overview'))
-      .waitForElementVisible("h2.routetitle")
-      .assert.containsText("h2.routetitle", "Articles")
-      .assert.urlEquals(`${base}/article`);
+    browser.url(`${base}`).click('a[href="/article"]', async () => {
+      browser
+        .saveScreenshot(sg(this, "overview"))
+        .waitForElementVisible("h2.routetitle")
+        .assert.containsText("h2.routetitle", "Articles")
+        .assert.urlEquals(`${base}/article`);
 
-      const checkArticle = async (id) => {
+      const checkArticle = async (id,article) => {
         await browser.click(`a[href="/article/${id}"]`, () => {
-          browser.waitForElementVisible("h2.routetitle")
-          .saveScreenshot(sg(this,`click-${id}`))
-          .assert.urlEquals(`${base}/article/${id}`);
+          browser
+            .waitForElementVisible("h2.routetitle")
+            .saveScreenshot(sg(this, `click-${id}`))
+            .assert.urlEquals(`${base}/article/${id}`)
+            .assert.containsText("h2.routetitle", `Article ${article.name}`)
         });
       };
 
-      await checkArticle('01');
-      await checkArticle('02');
+      for (const id of Object.keys(articles)) {
+        await checkArticle(id, articles[id]);
+      }
 
       browser.end();
     });
   });
-
-
 });
