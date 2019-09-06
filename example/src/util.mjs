52b4a0d4e274611ba0fb30862bcfaf4457547e54
Markus Felten <Markus.Felten@gmx.de>
2019-09-06 11:15:56 +0200

    test: separate data

--- example/src/util.mjs
+++ example/src/util.mjs
@@ -1,21 +1,6 @@
 import { readable } from "svelte/store";
+import { articles as _articles } from './data.mjs';
 
-export const _articles = {
-  "01": {
-    name: "Peanutbutter",
-    category: "staple",
-    price: 1.98
-  },
-  "02": { name: "Cheesecake", price: 2.0, category: "dessert" },
-  "03": { name: "Hot Dog", price: 2.0, category: "to go" },
-  "10": { name: "Pizza Quattro Stagioni", price: 8.0, category: "pizza" },
-  "11": { name: "Pizza Salami", price: 7.0, category: "pizza" },
-  "12": { name: "Pizza Hawaii", price: 7.0, category: "pizza" },
-  "13": { name: "Pizza Margherita", price: 5.0, category: "pizza" },
-  "14": { name: "Pizza Funghi", price: 7.0, category: "pizza" }
-};
-
-Object.keys(_articles).forEach(id => (_articles[id].id = id));
 
 export const articles = readable({}, set => {
   setTimeout(() => {
--- tests/plain-navigation-test.js
+++ tests/plain-navigation-test.js
@@ -3,16 +3,19 @@ import { Selector } from "testcafe";
 const articles = {
   "01": {
     name: "Peanutbutter",
-    price: 1.2
+    category: "staple",
+    price: 1.98
   },
-  "02": { name: "Cheesecake" },
-  "03": { name: "Hot Dog" },
-  "10": { name: "Pizza Quattro Stagioni" },
-  "11": { name: "Pizza Salami" },
-  "12": { name: "Pizza Hawaii" },
-  "13": { name: "Pizza Margherita" },
-  "14": { name: "Pizza Funghi" }
+  "02": { name: "Cheesecake", price: 2.0, category: "dessert" },
+  "03": { name: "Hot Dog", price: 2.0, category: "to go" },
+  "10": { name: "Pizza Quattro Stagioni", price: 8.0, category: "pizza" },
+  "11": { name: "Pizza Salami", price: 7.0, category: "pizza" },
+  "12": { name: "Pizza Hawaii", price: 7.0, category: "pizza" },
+  "13": { name: "Pizza Margherita", price: 5.0, category: "pizza" },
+  "14": { name: "Pizza Funghi", price: 7.0, category: "pizza" }
 };
+
+Object.keys(articles).forEach(id => (articles[id].id = id));
 
 const al = id => {
   return { path: `/article/${id}`, title: `Article ${articles[id].name}` };
