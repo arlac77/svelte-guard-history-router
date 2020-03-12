bb0f37d10abe711d41bdea568652550ffff2348a
Markus Felten <Markus.Felten@gmx.de>
2020-03-12 18:15:55 +0100

    docs: update gh-pages to work with firefox

--- example/rollup.config.js
+++ example/rollup.config.js
@@ -1,17 +1,18 @@
-import pkg from "../package.json";
-
 import dev from "rollup-plugin-dev";
 import svelte from "rollup-plugin-svelte";
 import resolve from "rollup-plugin-node-resolve";
+import { terser } from "rollup-plugin-terser";
 
 const port = 5000;
+const production = !process.env.ROLLUP_WATCH;
 
 export default {
   input: "example/src/index.mjs",
   output: {
     sourcemap: true,
     format: "esm",
-    file: `example/public/bundle.mjs`
+    file: "example/public/bundle.mjs",
+    plugins: [production && terser()]
   },
   plugins: [
     dev({
