fc35d8a234fe1c6cfe9c1b9418b9d0bca4bc77f5
Markus Felten <Markus.Felten@gmx.de>
2020-06-02 15:42:17 +0200

    fix: export TargetRoute

--- src/index.svelte
+++ src/index.svelte
@@ -14,9 +14,11 @@
   } from "./index.mjs";
   import Outlet from "./components/Outlet.svelte";
   import Link from "./components/Link.svelte";
+  import TargetRoute from "./components/TargetRoute.svelte";
   export {
     Outlet,
     Link,
+    TargetRoute,
     Router,
     Route,
     GuardedRoute,
