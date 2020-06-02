e8ccd46dd26a121c34d64d7e81b1ba1963b17d9c
Markus Felten <Markus.Felten@gmx.de>
2020-06-02 15:56:03 +0200

    fix: imports

--- src/components/TargetRoute.svelte
+++ src/components/TargetRoute.svelte
@@ -1,8 +1,10 @@
 <script>
-  import { route, link, active } from "svelte-guard-history-router";
+  import { link } from "../link.mjs";
+  import { active } from "../active.mjs";
+  import { route } from "../route.mjs";
 
-  export let router;
   export let href;
+  export let router;
   export let target;
 
   router.addRoute(route(href, target));
