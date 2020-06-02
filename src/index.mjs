e1ac6a8feddbe7b9010dccb4733ef1917323cf3f
Markus Felten <Markus.Felten@gmx.de>
2020-06-02 20:39:39 +0200

    feat: StoreRoute

--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,7 @@
   export * from './router.mjs';
   export * from './transition.mjs';
   export * from './route.mjs';
+  export * from './store-route.mjs';
   export * from './guard.mjs';
   export * from './waiting-guard.mjs';
   export * from './active.mjs';
