e166afef651fb5dffd855e6411ca3b5646e79c7b
Markus Felten <Markus.Felten@gmx.de>
2020-06-16 18:20:00 +0200

    example: bring back category articles

--- example/src/Category.svelte
+++ example/src/Category.svelte
@@ -1,17 +1,14 @@
 <script>
   import { Link } from "../../src/index.svelte";
   import { categoryRoute } from "./index.mjs";
-
 </script>
 
 {#if $categoryRoute}
   <h2 class="routetitle">Category {$categoryRoute.name}</h2>
 
-<!--
   {#each $categoryRoute.articles as article}
     <li>
       <Link href="/article/{article.id}">{article.name} ({article.id})</Link>
     </li>
   {/each}
-  -->
 {:else}No such category{/if}
