bc77c1b87a6ff61170c36376323fcfc57faa2052
Markus Felten <Markus.Felten@gmx.de>
2020-06-16 18:12:14 +0200

    feat: ObjectLink

---
+++ src/components/ObjectLink.svelte
@@ -0,0 +1,18 @@
+<script>
+  import Link from "./Link.svelte";
+  import { getContext } from "svelte";
+  import { ROUTER } from "../util.mjs";
+  const router = getContext(ROUTER);
+
+  export let object;
+
+  const path = router.pathFor(object);
+</script>
+
+{#if path}
+  <Link href={path}>
+    <slot />
+  </Link>
+{:else}
+  <slot name="noFound" />
+{/if}
--- src/index.svelte
+++ src/index.svelte
@@ -17,6 +17,7 @@
   } from "./index.mjs";
   import Outlet from "./components/Outlet.svelte";
   import Link from "./components/Link.svelte";
+  import ObjectLink from "./components/Link.svelte";
   import TargetRoute from "./components/TargetRoute.svelte";
   import TheRouter from "./components/TheRouter.svelte";
   export {
@@ -24,6 +25,7 @@
     TheRouter,
     Outlet,
     Link,
+    ObjectLink,
     Router,
     Route,
     GuardedRoute,
