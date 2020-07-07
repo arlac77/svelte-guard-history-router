6eba99a7cbf23dd794e93c9c50173f5312632649
Markus Felten <Markus.Felten@gmx.de>
2020-07-07 22:43:34 +0200

    test: wip

--- example/src/Article.svelte
+++ example/src/Article.svelte
@@ -7,14 +7,16 @@
   <h2 class="routetitle">Article {$articleRoute.name}</h2>
   <div>Id: {$articleRoute.id}</div>
   <div>Price: {$articleRoute.price}</div>
-  <ObjectLink object={$articleRoute.category}/>
+  <ObjectLink object={$articleRoute.category} />
 
-  <Link
-    href="/article/{('00' + (parseInt($articleRoute.id) + 1)).replace(/.*(\d\d)$/, '$1')}">
-    Next
-  </Link>
-  <Link
-    href="/article/{('00' + (parseInt($articleRoute.id) - 1)).replace(/.*(\d\d)$/, '$1')}">
-    Prev
-  </Link>
+  <div>
+    <Link
+      href="/article/{('00' + (parseInt($articleRoute.id) + 1)).replace(/.*(\d\d)$/, '$1')}">
+      Next
+    </Link>
+    <Link
+      href="/article/{('00' + (parseInt($articleRoute.id) - 1)).replace(/.*(\d\d)$/, '$1')}">
+      Prev
+    </Link>
+  </div>
 {:else}No such article{/if}
