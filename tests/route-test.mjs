98b0f59b75ef9a066be72ab94985904f35577976
Markus Felten <Markus.Felten@gmx.de>
2020-07-10 19:55:11 +0200

    test: route...

--- tests/route-test.mjs
+++ tests/route-test.mjs
@@ -5,10 +5,27 @@ test("route path", t => {
   const parentRoute = new SkeletonRoute();
   parentRoute.localPath = "/a";
 
-  const route = new SkeletonRoute();
+  t.is(parentRoute.path, "/a");
 
+  const route = new SkeletonRoute();
   route.parent = parentRoute;
   route.localPath = "/b";
 
   t.is(route.path, "/a/b");
+});
+
+
+
+test("route propertiesFor", t => {
+  const parentRoute = new SkeletonRoute();
+  parentRoute.localPath = "/a";
+  const route = new SkeletonRoute();
+  route.parent = parentRoute;
+  route.localPath = "/b";
+
+ // route.propertyMapping = { repository: "name" };
+
+
+  t.deepEqual(route.propertiesFor({ name: "repo1" }),undefined);
+
 });
