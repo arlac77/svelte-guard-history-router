2e56ce5ec4417ec57afaefbb99abbcdb0bcffe16
Markus Felten <Markus.Felten@gmx.de>
2020-07-25 16:56:51 +0200

    fix: cleanup

--- src/link.mjs
+++ src/link.mjs
@@ -3,14 +3,7 @@ import { findClosestAttribute } from "./util.mjs";
 export function link(node, router) {
   node.addEventListener("click", event => {
     event.preventDefault();
-
-    const href = findClosestAttribute(event.target, "href");
-
-    if (href === null) {
-      throw Error("Could not find corresponding href value");
-    }
-
-    router.push(href);
+    router.push(findClosestAttribute(event.target, "href"));
     return false;
   });
 }
