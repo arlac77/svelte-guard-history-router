69def643b5e37a1ba3f1a450783e47b7b7663224
Markus Felten <Markus.Felten@gmx.de>
2020-08-01 20:18:29 +0200

    fix: get rid of index.mjs (aggregate everything in index.svelte)

--- src/index.mjs
+++
@@ -1,7 +0,0 @@
-  export * from './base-router.mjs';
-  export * from './transition.mjs';
-  export * from './routes.mjs';
-  export * from './guard.mjs';
-  export * from './waiting-guard.mjs';
-  export * from './active.mjs';
-  export * from './link.mjs';
--- src/index.svelte
+++ src/index.svelte
@@ -1,19 +1,22 @@
 <script context="module">
+  import { BaseRouter } from "./base-router.mjs";
   import {
-    BaseRouter,
     SkeletonRoute,
     ObjectStoreRoute,
     IteratorStoreRoute,
-    ChildStoreRoute,
-    Transition,
+    ChildStoreRoute
+  } from "./routes.mjs";
+  import { Transition } from "./transition.mjs";
+  import {
     Guard,
     parallelGuard,
     sequenceGuard,
-    redirectGuard,
-    WaitingGuard,
-    active,
-    link
-  } from "./index.mjs";
+    redirectGuard
+  } from "./guard.mjs";
+  import { WaitingGuard } from "./waiting-guard.mjs";
+  import { active } from "./active.mjs";
+  import { link } from "./link.mjs";
+
   import Outlet from "./components/Outlet.svelte";
   import Link from "./components/Link.svelte";
   import ObjectLink from "./components/ObjectLink.svelte";
