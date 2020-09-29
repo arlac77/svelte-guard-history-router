ccb702d9e6e4facf0ab6eeb6d71c31fa84f29ae2
Markus Felten <Markus.Felten@gmx.de>
2020-09-29 21:17:53 +0200

    test: more transition ssserts

--- tests/router-nav-test.mjs
+++ tests/router-nav-test.mjs
@@ -4,6 +4,14 @@ import { setupRouter } from "./helpers/setup.mjs";
 test("router transition", async t => {
   const { router } = setupRouter();
 
+  let subscriptionPath;
+
+  router.subscribe(value => {
+  //  console.log("SUBSCRIPTION", value);
+  subscriptionPath = value.path;
+  });
+
+  t.is(history.length, 1);
   t.is(router.path, "/");
   t.falsy(router.transition);
 
@@ -12,9 +20,13 @@ test("router transition", async t => {
   t.truthy(router.transition);
   t.is(router.transition.path, "/master");
 
+  t.is(subscriptionPath,'/'); // still on old path
+
   await transition;
 
   t.falsy(router.transition);
 
   t.is(router.path, "/master");
+
+  t.is(history.length, 2);
 });
