03d392cf66aa496b7b51b87bf3512a892faf12ec
Markus Felten <Markus.Felten@db.com>
2020-09-30 12:31:26 +0200

    test: more asserts

--- tests/router-nav-test.mjs
+++ tests/router-nav-test.mjs
@@ -4,11 +4,12 @@ import { setupRouter } from "./helpers/setup.mjs";
 test("router transition", async t => {
   const { router } = setupRouter();
 
-  let subscriptionPath;
+  let subscriptionPath,subscriptionComponent;
 
-  router.subscribe(value => {
-    //console.log("SUBSCRIPTION", value.path);
-    subscriptionPath = value.path;
+  router.subscribe(router => {
+    //console.log("SUBSCRIPTION", router.path);
+    subscriptionPath = router.path;
+    subscriptionComponent = router.component;
   });
 
   t.is(subscriptionPath, "/");
@@ -16,24 +17,21 @@ test("router transition", async t => {
   t.is(history.length, 1);
   t.is(router.path, "/");
   t.falsy(router.component);
-
   t.falsy(router.transition);
 
   const transition = router.push("/master");
 
   t.truthy(router.transition);
   t.is(router.transition.path, "/master");
-
   t.is(subscriptionPath, "/"); // still on old path
 
   await transition;
 
   t.falsy(router.transition);
-
   t.is(router.path, "/master");
   t.is(router.component.name, "MasterComponent");
   t.is(subscriptionPath, "/master");
-
+  t.is(subscriptionComponent.name, "MasterComponent");
   t.is(history.length, 2);
   t.is(window.location.pathname, "/master");
 });
