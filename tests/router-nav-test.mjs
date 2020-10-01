e6da5279650e662247a83673610b39d28fb2f474
Markus Felten <Markus.Felten@gmx.de>
2020-10-02 00:13:41 +0200

    test: severla transitions in a row

--- tests/router-nav-test.mjs
+++ tests/router-nav-test.mjs
@@ -1,7 +1,7 @@
 import test from "ava";
 import { setupRouter } from "./helpers/setup.mjs";
 
-async function rtt(t, path, componentName) {
+async function rtt(t, items) {
   const { router } = setupRouter();
 
   let subscriptionPath, subscriptionComponent;
@@ -13,30 +13,42 @@ async function rtt(t, path, componentName) {
 
   const startPath = subscriptionPath;
   const startHistoryLength = history.length;
-  //t.is(history.length, 1);
+
   t.is(router.path, startPath);
   t.falsy(router.component);
-  t.falsy(router.transition);
-
-  const transition = router.push(path);
-
-  t.truthy(router.transition);
-  t.is(router.transition.path, path);
   t.is(subscriptionPath, startPath);
 
-  await transition;
+  let n = 0;
 
-  t.falsy(router.transition);
-  t.is(router.path, path);
-  t.is(router.component.name, componentName);
-  t.is(subscriptionPath, path);
-  t.is(subscriptionComponent.name, componentName);
-  t.is(history.length, startHistoryLength + 1);
-  t.is(window.location.pathname, path);
+  for (const item of items) {
+    n++;
+    const path = item.path;
+    const componentName = item.component;
+
+    t.falsy(router.transition);
+
+    const transition = router.push(path);
+
+    t.truthy(router.transition);
+    t.is(router.transition.path, path);
+
+    await transition;
+
+    t.falsy(router.transition);
+    t.is(router.path, path);
+    t.is(router.component.name, componentName);
+    t.is(subscriptionPath, path);
+    t.is(subscriptionComponent.name, componentName);
+    t.is(history.length, startHistoryLength + n);
+    t.is(window.location.pathname, path);
+  }
 }
 
-rtt.title = (providedTitle = "", path, componentName) =>
-  `router transition ${providedTitle} ${path}}`.trim();
+rtt.title = (providedTitle = "", items) =>
+  `router transition ${providedTitle} ${items.map(item => item.path)}}`.trim();
 
-test.serial(rtt, "/master", "MasterComponent");
-test.serial(rtt, "/master/1", "DetailComponent");
+test.serial(rtt, [{ path: "/master", component: "MasterComponent" }]);
+test.serial(rtt, [
+  { path: "/master/1", component: "DetailComponent" },
+  { path: "/master/2", component: "DetailComponent" }
+]);
