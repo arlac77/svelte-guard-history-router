de0be5d0b3cc870d514d581a264a7bf15a57721e
Markus Felten <Markus.Felten@gmx.de>
2020-10-01 23:27:02 +0200

    test: several transitions

--- tests/router-nav-test.mjs
+++ tests/router-nav-test.mjs
@@ -1,37 +1,42 @@
 import test from "ava";
 import { setupRouter } from "./helpers/setup.mjs";
 
-test("router transition", async t => {
+async function rtt(t, path, componentName) {
   const { router } = setupRouter();
 
-  let subscriptionPath,subscriptionComponent;
+  let subscriptionPath, subscriptionComponent;
 
   router.subscribe(router => {
-    //console.log("SUBSCRIPTION", router.path);
     subscriptionPath = router.path;
     subscriptionComponent = router.component;
   });
 
-  t.is(subscriptionPath, "/");
-
-  t.is(history.length, 1);
-  t.is(router.path, "/");
+  const startPath = subscriptionPath;
+  const startHistoryLength = history.length;
+  //t.is(history.length, 1);
+  t.is(router.path, startPath);
   t.falsy(router.component);
   t.falsy(router.transition);
 
-  const transition = router.push("/master");
+  const transition = router.push(path);
 
   t.truthy(router.transition);
-  t.is(router.transition.path, "/master");
-  t.is(subscriptionPath, "/"); // still on old path
+  t.is(router.transition.path, path);
+  t.is(subscriptionPath, startPath);
 
   await transition;
 
   t.falsy(router.transition);
-  t.is(router.path, "/master");
-  t.is(router.component.name, "MasterComponent");
-  t.is(subscriptionPath, "/master");
-  t.is(subscriptionComponent.name, "MasterComponent");
-  t.is(history.length, 2);
-  t.is(window.location.pathname, "/master");
-});
+  t.is(router.path, path);
+  t.is(router.component.name, componentName);
+  t.is(subscriptionPath, path);
+  t.is(subscriptionComponent.name, componentName);
+  t.is(history.length, startHistoryLength + 1);
+  t.is(window.location.pathname, path);
+}
+
+rtt.title = (providedTitle = "", path, componentName) =>
+  `router transition ${providedTitle} ${path}}`.trim();
+
+test.serial(rtt, "/master", "MasterComponent");
+test.serial(rtt, "/master/1", "DetailComponent");
