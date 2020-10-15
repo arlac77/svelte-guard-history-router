ef66965d0334af944ab0efca328cf230f939f275
Markus Felten <Markus.Felten@gmx.de>
2020-10-15 17:11:13 +0200

    test: fix typo

--- tests/app/src/Home.svelte
+++ tests/app/src/Home.svelte
@@ -2,7 +2,7 @@
 <p>Welcome to the router example code!</p>
 <p>Things to try:</p>
 <ul>
-    <li>Navigate around with the links and buttons (notice how certain links become active depending on the path</li>
+    <li>Navigate around with the links and buttons (notice how certain links become active depending on the path)</li>
     <li>Try pressing the browsers' back and forward buttons</li>
     <li>Manually change the URL's path</li>
     <li>Try refreshing the page</li>
