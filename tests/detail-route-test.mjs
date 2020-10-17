c28aa6f2686d093b800cc424343371b173e041d9
Markus Felten <Markus.Felten@gmx.de>
2020-10-17 03:24:02 +0200

    test: with ChildStoreRoute wih propertyMapping

--- tests/detail-route-test.mjs
+++ tests/detail-route-test.mjs
@@ -5,11 +5,17 @@ import { BaseRouter } from "../src/base-router.mjs";
 import { setupRouter } from "./helpers/setup.mjs";
 
 test("DetailRoute objectFor", async t => {
-  const parent = new SkeletonRoute("/master");
-  const child = new ChildStoreRoute("/detail/:id", { parent });
+  const values = [{ id: 1 }, { id: 2 }];
+  const parent = new SkeletonRoute("/master", {
+    iteratorFor: (transition, properties) => values
+  });
+  const child = new ChildStoreRoute("/detail/:id", {
+    parent,
+    propertyMapping: { id: "id" }
+  });
   const router = new BaseRouter([parent, child], "");
-  const transition = new Transition(router, "/master/detail/1");
+  const transition = new Transition(router, "/master/detail/7");
 
-  const o = await child.objectFor(transition, {});
-  t.is(o, undefined);
+  t.is(await child.objectFor(transition, { id: 7 }), undefined);
+  t.is(await child.objectFor(transition, { id: 2 }), values[1]);
 });
