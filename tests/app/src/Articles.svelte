794cfeb74b3819678da1daf2c104a553ea342d64
Markus Felten <Markus.Felten@gmx.de>
2020-11-03 23:32:54 +0100

    test: starting with query params

--- tests/app/src/Articles.svelte
+++ tests/app/src/Articles.svelte
@@ -4,14 +4,28 @@
   export let router;
 
   const route = router.route;
+
+  let filter;
 </script>
 
 <h2 class="routetitle">Articles</h2>
 
+<input
+  aria-label="filter"
+  maxlength="75"
+  size="32"
+  autocorrect="off"
+  autocapitalize="off"
+  id="filter"
+  type="text"
+  placeholder="Filter"
+  name="filter"
+  bind:value={filter} />
+
 <ul>
   {#each $route as article}
     <li>
-      <ObjectLink object={article} suffix="#price"/>
+      <ObjectLink object={article} suffix="#price" />
     </li>
   {/each}
 </ul>
