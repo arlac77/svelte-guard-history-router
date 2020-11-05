19637202ebe3b45847e68dbb06f156239eaf6a90
Markus Felten <Markus.Felten@gmx.de>
2020-11-05 01:21:09 +0100

    test: wip

--- tests/router-basics.mjs
+++ tests/router-basics.mjs
@@ -6,8 +6,11 @@ setupRouter();
 
 test("router basics", t => {
   const router = new BaseRouter([], "/base");
-  t.is(router.base, "/base");
-  t.is(router.path, "");
+  t.is(router.base, "/base", "given base");
+  t.is(router.path, "", "empty path");
+  t.deepEqual(router.params, {}, "no params");
+  t.is(router.route, undefined, "no route");
+  t.is(router.component, undefined, "no component");
   t.is(router.searchParams.get("q"), null);
 
   /*
