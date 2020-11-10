c805a7adb4ddb19b8721541b16c7e0e846c269d0
Markus Felten <Markus.Felten@gmx.de>
2020-11-10 14:39:47 +0100

    test: wip

--- tests/app/src/Home.svelte
+++ tests/app/src/Home.svelte
@@ -5,6 +5,7 @@
     <li>Navigate around with the links and buttons (notice how certain links become active depending on the path)</li>
     <li>Try pressing the browsers' back and forward buttons</li>
     <li>Manually change the URL's path</li>
+    <li>Copy paste the URL into a new brwoser window</li>
     <li>Try refreshing the page</li>
     <li>check router state</li>
 </ul>
