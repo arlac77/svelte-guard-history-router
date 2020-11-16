09a8438c3b1479c9590d261cece4f27587fbe28a
Markus Felten <Markus.Felten@gmx.de>
2020-11-16 02:41:42 +0100

    test: MasterRoute

--- tests/master-detail-route-test.mjs
+++ tests/master-detail-route-test.mjs
@@ -5,22 +5,50 @@ import { Transition } from "../src/transition.mjs";
 import { BaseRouter } from "../src/base-router.mjs";
 import { setupRouter } from "./helpers/setup.mjs";
 
-test("DetailRoute objectFor", async t => {
+function mdSetup() {
   const values = [{ id: 1 }, { id: 2 }];
-  const parent = new MasterRoute("/master", {
+  const master = new MasterRoute("/master", {
     iteratorFor: transition => values,
     propertyMapping: { did: "id" }
   });
-  const child = new DetailRoute("/detail/:did", {
-    parent
+  const detail = new DetailRoute("/detail/:did", {
+    parent: master
   });
 
-  const router = new BaseRouter([parent, child], "");
+  const router = new BaseRouter([master, detail], "");
+  return { router, master, detail, values };
+}
+
+test("MasterRoute fist last next previous", async t => {
+  const { router, master, values } = mdSetup();
+
+  const transition = new Transition(router, "/master");
+  await transition.start();
+
+  t.is(await master.first(), values[0]);
+  t.is(await master.last(), values[1]);
+  t.is(await master.next(values[0]), values[1]);
+  t.is(await master.previous(values[1]), values[0]);
+});
+
+test("MasterRoute objectFor", async t => {
+  const { router, master, values } = mdSetup();
+
   const transition = new Transition(router, "/master/detail/2");
-  
+  t.deepEqual(transition.params, { did: "2" });
+  await transition.start();
+
+  t.is(await master.objectFor(transition), values[1]);
+});
+
+test("DetailRoute objectFor", async t => {
+  const { router, detail, values } = mdSetup();
+
+  const transition = new Transition(router, "/master/detail/2");
+
   t.deepEqual(transition.params, { did: "2" });
 
   await transition.start();
 
-  t.is(await child.objectFor(transition), values[1]);
+  t.is(await detail.objectFor(transition), values[1]);
 });
