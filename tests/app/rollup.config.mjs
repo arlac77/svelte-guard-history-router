475af0f97bbfbce9c2036a56e1159cf41946820c
Markus Felten <Markus.Felten@gmx.de>
2020-11-26 18:17:31 +0100

    chore(rollup): cleanup

--- tests/app/rollup.config.mjs
+++ tests/app/rollup.config.mjs
@@ -1,5 +1,4 @@
 import postcss from "rollup-plugin-postcss";
-
 import virtual from "@rollup/plugin-virtual";
 import resolve from "@rollup/plugin-node-resolve";
 import dev from "rollup-plugin-dev";
@@ -15,18 +14,24 @@ export default {
     format: "esm",
     file: `${basedir}/public/bundle.main.mjs`
   },
-  plugins: [virtual({
-    "node-fetch": "export default fetch",
-    stream: "export class Readable {}",
-    buffer: "export class Buffer {}"
-  }), svelte(), resolve({
-    browser: true,
-    dedupe: importee =>
-      importee === "svelte" || importee.startsWith("svelte/")
-  }), dev({
-    port,
-    dirs: [`${basedir}/public`],
-    spa: `${basedir}/public/index.html`,
-    basePath: "/"
-  }), postcss()]
+  plugins: [
+    virtual({
+      "node-fetch": "export default fetch",
+      stream: "export class Readable {}",
+      buffer: "export class Buffer {}"
+    }),
+    svelte(),
+    postcss(),
+    resolve({
+      browser: true,
+      dedupe: importee =>
+        importee === "svelte" || importee.startsWith("svelte/")
+    }),
+    dev({
+      port,
+      dirs: [`${basedir}/public`],
+      spa: `${basedir}/public/index.html`,
+      basePath: "/"
+    })
+  ]
 };
