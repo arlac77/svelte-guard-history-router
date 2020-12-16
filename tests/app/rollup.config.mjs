8d4d52b0b328920792f43f171d4e9dec470c983b
Markus Felten <Markus.Felten@gmx.de>
2020-12-16 04:34:57 +0100

    test: use postcss extract

--- tests/app/rollup.config.mjs
+++ tests/app/rollup.config.mjs
@@ -3,7 +3,9 @@ import resolve from "@rollup/plugin-node-resolve";
 import dev from "rollup-plugin-dev";
 import svelte from "rollup-plugin-svelte";
 import postcss from "rollup-plugin-postcss";
+import postcssImport from "postcss-import";
 
+const production = !process.env.ROLLUP_WATCH;
 const basedir = "tests/app";
 const port = 5000;
 
@@ -20,18 +22,26 @@ export default {
       stream: "export class Readable {}",
       buffer: "export class Buffer {}"
     }),
-    svelte(),
-    postcss(),
+    svelte({
+      dev: !production
+    }),
+    postcss({
+      extract: true,
+      sourceMap: true,
+      minimize: production,
+      plugins: [postcssImport]
+    }),
     resolve({
       browser: true,
       dedupe: importee =>
         importee === "svelte" || importee.startsWith("svelte/")
     }),
-    dev({
-      port,
-      dirs: [`${basedir}/public`],
-      spa: `${basedir}/public/index.html`,
-      basePath: "/"
-    })
+    !production &&
+      dev({
+        port,
+        dirs: [`${basedir}/public`],
+        spa: `${basedir}/public/index.html`,
+        basePath: "/"
+      })
   ]
 };
