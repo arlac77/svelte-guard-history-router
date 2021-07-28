25519480bd1cc5d7819201fe36ab4f497c90fd57
Markus Felten <Markus.Felten@gmx.de>
2021-07-28 12:55:35 +0200

    style: simplfy exporrts

--- src/index.svelte
+++ src/index.svelte
@@ -1,18 +1,18 @@
 <script context="module">
-  import { BaseRouter } from "./base-router.mjs";
-  import { SkeletonRoute, ObjectStoreRoute } from "./routes.mjs";
-  import { MasterRoute } from "./master-route.mjs";
-  import { DetailRoute } from "./detail-route.mjs";
-  import { Transition } from "./transition.mjs";
-  import {
+  export { BaseRouter } from "./base-router.mjs";
+  export { SkeletonRoute, ObjectStoreRoute } from "./routes.mjs";
+  export { MasterRoute } from "./master-route.mjs";
+  export { DetailRoute } from "./detail-route.mjs";
+  export { Transition } from "./transition.mjs";
+  export {
     Guard,
     parallelGuard,
     sequenceGuard,
     redirectGuard
   } from "./guard.mjs";
-  import { WaitingGuard } from "./waiting-guard.mjs";
-  import { active } from "./active.mjs";
-  import { link } from "./link.mjs";
+  export { WaitingGuard } from "./waiting-guard.mjs";
+  export { active } from "./active.mjs";
+  export { link } from "./link.mjs";
 
   import Outlet from "./components/Outlet.svelte";
   import Link from "./components/Link.svelte";
@@ -20,25 +20,5 @@
   import NamedObjectLink from "./components/NamedObjectLink.svelte";
   import Route from "./components/Route.svelte";
   import Router from "./components/Router.svelte";
-  export {
-    Route,
-    Router,
-    Outlet,
-    Link,
-    ObjectLink,
-    NamedObjectLink,
-    BaseRouter,
-    SkeletonRoute,
-    MasterRoute,
-    DetailRoute,
-    ObjectStoreRoute,
-    Transition,
-    Guard,
-    parallelGuard,
-    sequenceGuard,
-    redirectGuard,
-    WaitingGuard,
-    active,
-    link
-  };
+  export { Route, Router, Outlet, Link, ObjectLink, NamedObjectLink };
 </script>
