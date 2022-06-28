514cf8fe2b1487ebd42822a8f4001366f1eb9d5a
Markus Felten <Markus.Felten@gmx.de>
2022-06-28 15:33:46 +0200

    fix: use private vars

--- src/waiting-guard.mjs
+++ src/waiting-guard.mjs
@@ -6,19 +6,21 @@ import { Guard } from "./guard.mjs";
  * @param {number} rampUpTime initial delay for the componnt to show up
  */
 export class WaitingGuard extends Guard {
+  
+  #component;
+  #rampUpTime;
+  
   constructor(component, rampUpTime = 300) {
     super();
-    Object.defineProperties(this, {
-      component: { value: component },
-      rampUpTime: { value: rampUpTime }
-    });
+    this.#component = component;
+    this.#rampUpTime = rampUpTime;
   }
 
   async enter(transition) {
     transition.timeoutID = setTimeout(() => {
       transition.timeoutID = undefined;
-      transition.component = this.component;
-    }, this.rampUpTime);
+      transition.component = this.#component;
+    }, this.#rampUpTime);
   }
 
   async leave(transition) {
