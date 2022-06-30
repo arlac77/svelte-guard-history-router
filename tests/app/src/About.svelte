714c5adcf17c8762f3369772b292669a2b8cf939
Markus Felten <Markus.Felten@gmx.de>
2022-06-30 20:50:49 +0200

    test: separate title from version

--- tests/app/src/About.svelte
+++ tests/app/src/About.svelte
@@ -1,5 +1,8 @@
 <script>
   import { name, version } from "./constants.mjs";
 </script>
-<h2 class="routetitle">About {name} {version}</h2>
+<h2 class="routetitle">About</h2>
+<div>
+{name} {version}
+</div>
 Example to show all features of <a href="https://github.com/arlac77/svelte-guard-history-router"><b>svelte-guard-histroy-router</b></a>
