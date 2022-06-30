866e50584b5df3ce9ccd2e3e12bed236d1ef95df
Markus Felten <Markus.Felten@gmx.de>
2022-06-30 20:45:11 +0200

    fix: show name + version in example

--- tests/app/src/About.svelte
+++ tests/app/src/About.svelte
@@ -1,2 +1,5 @@
-<h2 class="routetitle">About</h2>
+<script>
+  import { name, version } from "./constants.mjs";
+</script>
+<h2 class="routetitle">About {name} {version}</h2>
 Example to show all features of <a href="https://github.com/arlac77/svelte-guard-history-router"><b>svelte-guard-histroy-router</b></a>
