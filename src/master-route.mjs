4f96d4a2244604e10b31e95f09350890fcdc03b9
Markus Felten <Markus.Felten@gmx.de>
2022-07-01 17:40:42 +0200

    fix: use value

--- src/master-route.mjs
+++ src/master-route.mjs
@@ -20,8 +20,8 @@ export class MasterRoute extends SkeletonRoute {
 
     const values = [];
 
-    for await (const e of await this.iteratorFor(transition)) {
-      values.push(e);
+    for await (const value of await this.iteratorFor(transition)) {
+      values.push(value);
     }
 
     this.value = values;
