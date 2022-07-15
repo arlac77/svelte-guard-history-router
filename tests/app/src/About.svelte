ffefc8aba8ebd1a97aa02066ed4173e25c41e45d
Markus Felten <Markus.Felten@gmx.de>
2022-07-15 12:40:28 +0200

    style: example

--- tests/app/src/About.svelte
+++ tests/app/src/About.svelte
@@ -4,10 +4,8 @@
 
 <h2 class="routetitle">About</h2>
 <div>
-  {name}
-  {version}
+  Example to show all features of <a
+    href="https://github.com/arlac77/svelte-guard-history-router"
+    ><b>{name}@{version}</b></a
+  >
 </div>
-Example to show all features of<a
-  href="https://github.com/arlac77/svelte-guard-history-router"
-  ><b>{name}</b></a
->
