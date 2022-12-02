79a05798849901f221a2273c2bc7cc38d731a6de
Markus Felten <Markus.Felten@gmx.de>
2022-12-02 10:37:07 +0100

    test: wip

--- tests/cafe/query-params.js
+++ tests/cafe/query-params.js
@@ -1,7 +1,8 @@
 import { Selector } from "testcafe";
 import { base, login } from "./helpers/util.js";
 
-fixture`Query Params`.page`${base}article?filter:name=Pizza`;
+fixture`Query Params`
+  .page`${base}article?filter:name=Pizza&sort:name=descending`;
 
 test("query params extracted", async t => {
   const title = Selector(".routetitle");
@@ -9,8 +10,9 @@ test("query params extracted", async t => {
   await login(t);
 
   await t.expect(title.innerText).eql("Articles");
-  
+
   await t.expect(Selector("#filter_name").value).eql("Pizza");
+  //await t.expect(Selector("#name").withAttribute("class", "aria-sort").exists);
 
   /*
   await t.typeText("#filter_name", "Hot", { replace: true });
