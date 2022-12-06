e12a76bb420bffe730f797a0285b3ba11f966151
Markus Felten <Markus.Felten@gmx.de>
2022-12-06 18:04:15 +0100

    test: wip

--- tests/helpers/fake-browser.mjs
+++ tests/helpers/fake-browser.mjs
@@ -1,27 +1,59 @@
 if (!globalThis.window) {
-
   const base = "http://example.com";
+  const listeners = { popstate: [], routeLink: [] };
 
   const window = {
-    addEventListener() {},
+    addEventListener(slot, fn) {
+      listeners[slot].push(fn);
+    },
 
     location: new URL(base)
   };
 
   globalThis.window = window;
 
-  const state = [];
+  let cs = 0;
+  const states = [{ state: undefined, url: base }];
 
   const history = {
-    back() {},
-    forward() {},
-    get state() { return state[state.length - 2]; },
-    get length() { return state.length; },
-
-    replaceState(a, b, location) {
-      window.location = new URL(location, base);
+    async back() {
+      if (cs > 0) {
+        cs = cs - 1;
+        window.location = states[cs].url;
+      }
     },
-    pushState() {}
+    async forward() {
+      if (cs < states.length - 1) {
+        cs = cs + 1;
+        window.location = states[cs].url;
+      }
+    },
+    async go(off) {
+      if (cs + off < 0 || cs + off > states.length - 1) {
+        return;
+      }
+
+      cs += off;
+      window.location = states[cs].url;
+    },
+
+    get state() {
+      return states[state.length - 2];
+    },
+    get length() {
+      return states.length;
+    },
+
+    replaceState(state, unused, url) {
+      url = new URL(url, base);
+      states[cs] = { state, url };
+      window.location = url;
+    },
+    pushState(state, unused, url) {
+      cs += 1;
+      states.push({ state, url });
+      window.location = url;
+    }
   };
 
   globalThis.history = history;
