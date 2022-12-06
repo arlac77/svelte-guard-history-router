2d421a84e841fd7b5f87af6c041454257e37f4b5
Markus Felten <Markus.Felten@gmx.de>
2022-12-06 17:23:19 +0100

    test: wip

--- tests/helpers/fake-browser.mjs
+++ tests/helpers/fake-browser.mjs
@@ -10,8 +10,14 @@ if (!globalThis.window) {
 
   globalThis.window = window;
 
+  const state = [];
+
   const history = {
     back() {},
+    forward() {},
+    get state() { return state[state.length - 2]; },
+    get length() { return state.length; },
+
     replaceState(a, b, location) {
       window.location = new URL(location, base);
     },
